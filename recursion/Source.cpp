249c77b294bda7e7bd387f6451a680d8e847d178
Евгений Суслопаров <suslegept@yahoo.com>
2023-08-30 17:27:29 +0300

    recursion

---
+++ recursion/Source.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void elevator(int floor);
+
+void main()
+{
+
+	setlocale(LC_ALL, "");
+
+	//cout << "Hello World";
+	//main();
+	int n;
+	cout << "введите номер этажа"; cin >> n;
+	elevator(n);
+
+}
+void elevator(int floor)
+{
+	if (floor == 0)
+	{
+		cout << "Вы в подвале" << endl;
+		return;
+	}
+	cout << "Вы на " << floor << " этаже\n";
+	elevator(floor-1);
+	cout << "Вы на " << floor << " этаже\n";
+
+}
