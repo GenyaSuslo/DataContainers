e3e57aecbbde555ab77737263a8c5cdd5edd970a
Евгений Суслопаров <suslegept@yahoo.com>
2023-09-01 17:27:53 +0300

    Uniquetree

--- Binari3/main.cpp
+++ Binari3/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Tree
 {
+protected:
 	class Element
 	{
 		int Data;
@@ -18,8 +19,14 @@ class Tree
 		{
 			cout << "EDestructor:\t" << this << endl;
 		}
+		friend class Tree;
+		friend class UniqueTree;
 	}*Root;
 public:
+	Element* getRoot()
+	{
+		return Root;
+	}
 	Tree() :Root(nullptr)
 	{
 		cout << "TConstructor:\t" << this << endl;
@@ -28,13 +35,115 @@ public:
 	{
 		cout << "TDestructor:\t" << this << endl;
 	}
-	void insert()
-};
+	void insert(int Data,Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+		/*insert(Data, Root->pLeft);не нужны,выдают бесконечный цикл
+		insert(Data, Root->pRight);*/
+	}
+	int Sum(Element* Root)
+	{
+		/*if (Root == nullptr)return 0;else*/ 
+			return Root==nullptr? 0 : Sum(Root->pLeft) + Sum(Root->pRight)+Root->Data;
+	}
+	
+	int Count(Element* Root)
+	{
+		return Root == nullptr ? 0 : Count(Root->pLeft)+Count(Root->pRight)+1;
 
+	}
+	double Avg()
+	{
+		return (double)Sum(Root) / Count(Root);
+	}
+	int minValue(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+		/*if (Root->pLeft == nullptr)return Root->Data;
+		else return minValue(Root->pLeft);*/
+	}
+	int maxValue(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		return Root->pRight == nullptr ? Root->Data : maxValue(Root->pRight);
+		/*if (Root->pRight == nullptr)return Root -> Data;
+		else return maxValue(Root->pRight);*/
+	}
+	
+	void print(Element* Root)
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << "\t";
+		print(Root->pRight);
+	}
+};
+class UniqueTree:public Tree
+{
+	public:
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else if(Data>Root->Data)
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+		/*insert(Data, Root->pLeft);не нужны,выдают бесконечный цикл
+		insert(Data, Root->pRight);*/
+	}
+
+
+
+
+};
 void main()
 {
 	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер дерева: "; cin >> n;
+	
+	Tree tree;
+	for (int i = 0; i < n; i++)
+	{
+		tree.insert(rand() % 100,tree.getRoot());
+	}
+	tree.print(tree.getRoot());
+	cout << endl;
+	cout << "Минимальное значение в дереве: " << tree.minValue(tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.getRoot()) << endl;
+	cout << "Сумма элементов дерева: \t\t" << tree.Sum(tree.getRoot()) << endl;
+	cout << "Колличество элементов дерева: \t\t" << tree.Count(tree.getRoot()) << endl;
+	cout << "Среднее-Арифметическое элементов дерева: " << tree.Avg() << endl;
 
-
-
+	UniqueTree u_tree;
+	for (int i = 0; i < n; i++)
+	{
+		u_tree.insert(rand() % 100, u_tree.getRoot());
+	}
+	u_tree.print(u_tree.getRoot());
+	cout << endl;
+	cout << "Минимальное значение в дереве: " << u_tree.minValue(u_tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << u_tree.maxValue(u_tree.getRoot()) << endl;
+	cout << "Сумма элементов дерева: \t" <<u_tree.Sum(u_tree.getRoot()) << endl;
+	cout << "Колличество элементов дерева: \t" << u_tree.Count(u_tree.getRoot()) << endl;
+	cout << "Среднее-Арифметическое элементов дерева: " << u_tree.Avg() << endl;
 }
